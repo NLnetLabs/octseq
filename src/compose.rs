418fbf354c710050a5861252cc8b161fd886cb73
Martin Hoffmann <martin@nlnetlabs.nl>
2022-08-16 12:06:25 +0200

    Remove unused compose module.

--- src/compose.rs
+++
@@ -1,8 +0,0 @@
-//! Constructing octets sequences from data.
-//!
-//! Composing encoded data always happens directly into an octets builder.
-//! Therefore, no `Composer` type is necessary. This module only defines a
-//! trait [`Compose`] which is used as an extension trait to provide
-//! `compose` methods for built-in types.
-
-// XXX Add the Compose trait or remove the module.
